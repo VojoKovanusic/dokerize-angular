28fd192121d859da1b4e15828385a9e01f80bfba
vojo <mladisumar@gmail.com>
2020-09-01 19:55:39 +0200

    Dadd ang material and boodstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
